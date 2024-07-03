e5afc4877d20f414a935cd38d1f516f8dd6049cd
leeruien <ljo312342@gmail.com>
2024-07-03 16:29:11 +0800

    Fixed formatting of bot reply

--- src/App.js
+++ src/App.js
@@ -28,42 +28,9 @@ function App(){
       sender: 'user'
     };
     const userMessageString = typeof userMessage === 'string' ? userMessage : String(userMessage);
-    //try {
-      // const payload = {
-      //   body: JSON.stringify({ input_text: userMessageString }) 
-      // };
-      console.log('after payload')
-      /*
-       const response = await axios.post(
-         'https://vqu1ejiuvl.execute-api.ap-southeast-1.amazonaws.com/qnembed', 
-        payload
-       ); 
-       ;*/
-       // send user's message to the api end point
-      //  await
-      //   fetch(' https://nhc6dafwmk.execute-api.ap-southeast-1.amazonaws.com/question_input', {
-      //       mode: 'cors',
-      //       method: 'POST',
-      //        body: JSON.stringify({
-      //          input_text: userMessageString }),
-      //       headers: {
-      //           'Content-Type': 'application/json'
-      //       }
-      //   }).catch(function (error) {
-      //       console.log("Error comunicating to server");
-      //   })
-      //   console.log("after posting api")
-        
-      //   const response = await fetch('http://13.212.53.73:8080/api', {
-      //       mode: 'cors',
-      //       method: 'GET',
-      //       headers: {
-      //           'Content-Type': 'application/json'
-      //       }
-      //   }).catch(function (error) {
-      //       console.log("Error comunicating to server", error.response);
-      //   })
-        const response = await fetch('http://13.213.16.238:8080/api', {
+    console.log('after payload')
+      // change api endpoint url ip to public ipv4 address of chromadb ec2 instance  
+        const response = await fetch('http://13.212.151.156:8080/api', { 
           mode: 'cors',
           method: 'POST',
           body: JSON.stringify({
@@ -75,65 +42,66 @@ function App(){
           console.log("Error comunicating to server", error.response);
       })
 
-        console.log("after flask api")
-      // if (!response.ok) {
-      //     throw new Error('Failed to send message: ' + response.statusText);
-      // }
-      
+      console.log("after flask api")      
       console.log('API Response: success', response.ok);
       console.log('API Response: success', response.data);
       const responseData = await response.json()
-      console.log('API Response: success', responseData);
-        //return response.data; // Return response data if you need it
-    // } catch (error) {
-    //     console.error('Error sending user message to API:', error);
-    //     console.error('Error response from API:', error.response);
-    //     // Display error message to user or handle the error appropriately
-    //     throw error; // Throw error to be handled by the caller if needed
-    // }
-    
-    // if api gateway returns after done dont process the message
-    
+      console.log('API Response: success', responseData);    
     setIsWaitingForResponse(true);
     setMessages(prevMessages => [...prevMessages, userMessageObj]);    
     //get generated text
     let answer = responseData.generated_text || '';
     console.log("this is answer", answer)
-    //replace special characters with space
-    if (answer.charAt(0) === '.' || answer.charAt(0) === '=' || answer.charAt(0) === ',') {
-      answer = answer.substr(1);
-    }    
+    // replace special characters in strings   
     answer = answer.replaceAll(/=/g, '')
-                    .replaceAll(/[\r\n]+/g, ' ')
-                    .replaceAll(/\*/g, '')
-                    .replaceAll(/".\n"/g, '')
-                    .replaceAll(/"outputs":\s*\[|\],?\s*"inputs":\s*\[|\]\s*{/g, '')
-                    .replaceAll(/{"inputs":\s*\[.*?\]}|{"outputs":\s*\[.*?\]}/g, '')
-                    .replaceAll(/{"inputs":\s*\[.*?\}|{"outputs":\s*\[.*?\]}/g, '')
-                    .replaceAll(/<|eot_id|><|start_header_id|>assistant<|end_header_id|>/g, '')
-                    .replaceAll(/<|eot_id|><|start_header_id|>assistant/g, '')
+                    // .replaceAll(/[\r\n]+/g, ' ')
+                    // .replaceAll(/\*/g, '')
+                    // .replaceAll(/-/g, '')
+                    // .replaceAll(/".\n"/g, '')
+                    // .replaceAll(/"outputs":\s*\[|\],?\s*"inputs":\s*\[|\]\s*{/g, '')
+                    // .replaceAll(/{"inputs":\s*\[.*?\]}|{"outputs":\s*\[.*?\]}/g, '')
+                    // .replaceAll(/{"inputs":\s*\[.*?\}|{"outputs":\s*\[.*?\]}/g, '')
                     .replace(/\[.*?\]/g, '')
-                    .replaceAll(/\bassistant\b/gi, '')
-                    .replaceAll(/!|\|+/g, '')
+                    .replace(/\{.*?\}/g, '')
+                    // .replaceAll(/\bassistant\b/gi, '')
+                    // .replaceAll(/!|\|+/g, '')
+                    .replaceAll(/^\s*>\s*/g, '')
                     .replaceAll(/[[\]{}"]|/g, '').trim();
 
-    console.log("this is answer 2", answer)    
-    let sentences = answer.split(/(?<=[.!?])\s+/); //sentences is array 
+    console.log("this is answer 2", answer)  
+    while (answer.charAt(0) === '.' || 
+       answer.charAt(0) === '=' || 
+       answer.charAt(0) === ',' || 
+       answer.charAt(0) === '-' || 
+       answer.charAt(0) === ':'|| 
+       answer.charAt(0) === '>'||  
+       answer.charAt(0) === ';') {
+    answer = answer.substr(1);
+    answer = answer.trim();
+    }  
+  let lastSentence = answer.search(/([.!?])(?=[^.!?]*$)/);
+
+  if (lastSentence !== -1) {
+    // Keep everything up to and including the last complete sentence
+    answer = answer.substring(0, lastSentence + 1);
+    answer = answer.trim();
+  }
+
+
+    // let sentences = answer.split(/(?<=[.!?])\s+/); //sentences is array 
     
-    //remove the truncated part of answer
-    if (sentences.length > 0 && !/[.!?]$/.test(sentences[sentences.length - 1])) {
-      sentences.pop();
-    } 
+    // //remove the truncated part of answer
+    // if (sentences.length > 0 && !/[.!?]$/.test(sentences[sentences.length - 1])) {
+    //   sentences.pop();
+    // } 
   
-    let botAnswer = sentences.join(' ').trim();
-    if (botAnswer.charAt(0) === '.' || botAnswer.charAt(0) === '=' || botAnswer.charAt(0) === ',' || botAnswer.charAt(0) === '-') {
-      botAnswer = botAnswer.substr(1);
-    } 
+    // let botAnswer = sentences.join(' ').trim();
+    // console.log("this is botanswer", botAnswer)
     
     console.log('before bot reply');
-    console.log(typeof botAnswer)
+    // console.log(typeof botAnswer)
     const botReply={
-      text: botAnswer, // insert here
+      text: answer, // insert here
       sender: 'bot'
       };
     console.log('after bot reply');
@@ -162,9 +130,10 @@ function App(){
   
   return(
     <div className='center-border' style={{justifyContent:'space-between'}}>    
-      <div className="header-border">
+      <div className="header-border" >
         <button className='heading-container'onClick={handleRefresh} style={{display: 'inline-block', marginRight: '20px'}}><FontAwesomeIcon icon={ faRefresh } /></button>
-        <h1 className='heading-container'style={{textAlign: 'center', display:'inline-block'}}>Ask me anything!</h1>                         
+        <h1 className='heading-container'style={{textAlign: 'center', display:'inline-block'}}>AI Chatbot</h1>     
+        {/* <img width="40" height="40" src="https://img.icons8.com/stickers/100/chatbot.png" alt="chatbot"/>              */}
           <div className='message-container' ref={lastMessage}>   
           <ChatForm handleUserMessage={handleUserMessage} messages={messages} isWaitingForResponse={isWaitingForResponse} />  
           {messages.map((message, index)=>(
@@ -175,25 +144,22 @@ function App(){
                 {message.text}
               </div>
             </div>
-        ))}
-          </div>
-      
+          ))}
+          </div>     
       </div>        
-    </div>      
-    
-    
+    </div>        
   );
 }
 
+// function to handle input box 
 function ChatForm({handleUserMessage, isWaitingForResponse}){  
   const [input, setInput] = useState('');
   let handleSubmit = null;
-  handleSubmit = (e) => { //function passed down, calls handleUserMessage
+  handleSubmit = (e) => { 
     e.preventDefault();
     if (!input.trim()) return;
     handleUserMessage(input);
     setInput('');
-    //setIsWaitingForResponse(true);
   };
 
   const handleChange = (e) =>{
@@ -206,8 +172,7 @@ function ChatForm({handleUserMessage, isWaitingForResponse}){
         <input type="text" placeholder='Type here...' 
         value={input} onChange={handleChange}/>
         <button disabled={isWaitingForResponse || input.trim()===''
-       } style={{alignContent:'flex-end'}} className='input' ><FontAwesomeIcon icon = {faPaperPlane}  size="2x"/></button>
-        
+       } style={{alignContent:'flex-end'}} className='input' ><FontAwesomeIcon icon = {faPaperPlane}  size="2x"/></button>        
       </div>
     </form>
   );
